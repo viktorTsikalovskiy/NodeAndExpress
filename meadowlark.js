0abd4a8c516ea1d8cf919e8e497e56f9edacbba6
stsykalovskyi <stsykalovskyi@singlehop.com>
2016-09-08 18:45:42 +0300

    first commit

---
+++ meadowlark.js
@@ -0,0 +1,51 @@
+/**
+ * Created by sergey on 08.09.16.
+ */
+const express = require("express"),
+    app = express(),
+    handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "/public"));
+
+
+let fortunes = [
+    "Победи свои страхи, или они победят тебя.",
+    "Рекам нужны истоки.",
+    "Не бойся неведомого.",
+    "Тебя ждет приятный сюрприз.",
+    "Будь проще везде, где только можно."
+];
+
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/about', function(req,res){
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {
+        fortune: randomFortune
+    });
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.' );
+});
