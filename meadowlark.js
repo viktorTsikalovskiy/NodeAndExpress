3e974a6e4663951c57c74b7e0dcd95281fce9736
stsykalovskyi <stsykalovskyi@singlehop.com>
2016-09-13 21:20:03 +0300

    commit

--- meadowlark.js
+++ meadowlark.js
@@ -5,92 +5,125 @@
 const express = require("express"),
     app = express(),
     fortune = require("./lib/fortunes"),
+    formidable = require('formidable'),
     handlebars = require('express-handlebars')
-    .create({
-        defaultLayout: 'main',
-        helpers: {
-            section: function(name, options){
-                if(!this._sections) this._sections = {};
-                this._sections[name] = options.fn(this);
-                return null;
+        .create({
+            defaultLayout: 'main',
+            helpers: {
+                section: function (name, options) {
+                    if (!this._sections) this._sections = {};
+                    this._sections[name] = options.fn(this);
+                    return null;
+                }
             }
-        }
-    });
+        });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.use(function(req, res, next){
-    if(!res.locals.partials) res.locals.partials = {};
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
     res.locals.partials.weatherContext = getWeatherData();
     next();
 });
-app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('body-parser').urlencoded({extended: true}));
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
     next();
 });
+////////////////////////////////////////////////
+
 app.get('/', function(req, res) {
     res.render('home');
 });
 app.get('/about', function(req,res){
     res.render('about', {
         fortune: fortune.getFortune(),
-        pageTestScript: "/qa/tests-about.js"
-    });
+        pageTestScript: '/qa/tests-about.js'
+    } );
 });
-app.get('/headers', function(req,res){
-    res.set('Content-Type','text/plain');
-    var s = '';
-    for(var name in req.headers)
-        s += name + ': ' + req.headers[name] + '\n';
-    res.send(s);
-});
-
 app.get('/tours/hood-river', function(req, res){
     res.render('tours/hood-river');
 });
+app.get('/tours/oregon-coast', function(req, res){
+    res.render('tours/oregon-coast');
+});
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
+});
+app.get('/jquery-test', function(req, res){
+    res.render('jquery-test');
 });
 app.get('/nursery-rhyme', function(req, res){
     res.render('nursery-rhyme');
 });
 app.get('/data/nursery-rhyme', function(req, res){
     res.json({
-        animal: 'бельчонок',
-        bodyPart: 'хвост',
-        adjective: 'пушистый',
-        noun: 'черт',
+        animal: 'squirrel',
+        bodyPart: 'tail',
+        adjective: 'bushy',
+        noun: 'heck',
     });
 });
-app.get('/test', function(req, res){
+app.get('/thank-you', function(req, res){
+    res.render('thank-you');
+});
+app.get('/newsletter', function(req, res){
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process', function(req, res){
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+app.get('/test', function (req, res) {
     res.render('jquery-test');
 });
 
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.status(404);
     res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
 });
 
 
-function getWeatherData(){
+function getWeatherData() {
     return {
         locations: [
             {
