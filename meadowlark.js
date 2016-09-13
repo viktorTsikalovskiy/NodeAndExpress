d94aed6ca3307c01b162e5ddfa49382ff7cfa0be
stsykalovskyi <stsykalovskyi@singlehop.com>
2016-09-13 15:42:24 +0300

    Commit

--- meadowlark.js
+++ meadowlark.js
@@ -6,10 +6,24 @@ const express = require("express"),
     app = express(),
     fortune = require("./lib/fortunes"),
     handlebars = require('express-handlebars')
-    .create({ defaultLayout: 'main' });
+    .create({
+        defaultLayout: 'main',
+        helpers: {
+            section: function(name, options){
+                if(!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weatherContext = getWeatherData();
+    next();
+});
+app.use(require('body-parser').urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + "/public"));
@@ -28,7 +42,6 @@ app.get('/about', function(req,res){
         pageTestScript: "/qa/tests-about.js"
     });
 });
-app.disable('x-powered-by');
 app.get('/headers', function(req,res){
     res.set('Content-Type','text/plain');
     var s = '';
@@ -42,6 +55,20 @@ app.get('/tours/hood-river', function(req, res){
 });
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
+});
+app.get('/nursery-rhyme', function(req, res){
+    res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+    res.json({
+        animal: 'бельчонок',
+        bodyPart: 'хвост',
+        adjective: 'пушистый',
+        noun: 'черт',
+    });
+});
+app.get('/test', function(req, res){
+    res.render('jquery-test');
 });
 
 // 404 catch-all handler (middleware)
@@ -61,3 +88,32 @@ app.listen(app.get('port'), function(){
     console.log( 'Express started on http://localhost:' +
         app.get('port') + '; press Ctrl-C to terminate.' );
 });
+
+
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Портленд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Сплошная облачность ',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Бенд',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Малооблачно',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Манзанита',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Небольшой дождь',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
